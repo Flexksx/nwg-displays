5517224412140e8d7b9d97e54b24afd755b1fbec
Flexksx <ccretu@extole.com>
2026-02-05 09:00:33 +0200

    update with pyproject

--- flake.nix
+++ flake.nix
@@ -1,53 +1,117 @@
 {
-  description = "An output management utility for the sway Wayland compositor, inspired by wdisplays and wlay.";
+  description = "Modernized nwg-displays with uv and nix";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, ...  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      # TODO: This probably works on more linux architectures but I haven't tested them
-      systems = ["x86_64-linux"];
-      imports = [];
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      uv2nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-      perSystem = { config, inputs', pkgs, system, ... }:
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+          python = pkgs.python314;
+
+          pythonSet =
+            (pkgs.callPackage inputs.pyproject-nix.build.packages {
+              inherit python;
+            }).overrideScope
+              (
+                pkgs.lib.composeManyExtensions [
+                  inputs.pyproject-build-systems.overlays.default
+                  workspace.mkPyprojectOverlay
+                  { sourcePreference = "wheel"; }
+                  (final: prev: { })
+                ]
+              );
+
+          runtimeDeps = with pkgs; [
+            gtk3
+            gtk-layer-shell
+            gobject-introspection
+            pango
+            gdk-pixbuf
+            atk
+          ];
+        in
         {
           packages = rec {
             default = nwg-displays;
-            nwg-displays = pkgs.python3Packages.buildPythonApplication
-            rec {
+
+            nwg-displays = pkgs.stdenv.mkDerivation {
               pname = "nwg-displays";
               version = "0.1.4";
-              doCheck = false;
-              src = self;
+              src = ./.;
 
               nativeBuildInputs = [
+                pkgs.makeWrapper
                 pkgs.wrapGAppsHook
-                pkgs.gobject-introspection
               ];
+              buildInputs = runtimeDeps;
 
-              buildInputs = with pkgs; [
-                gtk3
-              ];
-
-              propagatedBuildInputs = with pkgs; [
-                pango
-                gtk-layer-shell
-                gdk-pixbuf
-                atk
-                python3Packages.i3ipc
-                python3Packages.pygobject3
-                python3Packages.gst-python
-              ];
-
-              dontWrapGApps = true;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp -r ${pythonSet.mkVirtualEnv "nwg-env" workspace.deps.default} $out/venv
+                ln -s $out/venv/bin/nwg-displays $out/bin/nwg-displays
+              '';
 
               preFixup = ''
-                makeWrapperArgs+=("''${gappsWrapperArgs[@]}");
+                makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
               '';
             };
           };
+
+          devShells.default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                uv
+                python
+                pkg-config
+              ]
+              ++ runtimeDeps;
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeDeps}:$LD_LIBRARY_PATH"
+              if [ -f "pyproject.toml" ]; then
+                uv sync
+              fi
+            '';
+          };
         };
-      };
+    };
 }
